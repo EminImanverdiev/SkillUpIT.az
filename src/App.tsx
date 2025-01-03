45e582c593ddf25a6df82d3110c49db823c133ca
Afsana1999 <afsanahasimovaa@gmail.com>
2025-01-03 15:17:04 +0400

    funfact section done

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,43 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
-import './index.css';
-import About from './pages/About/About';
-import Home from './pages/Home/Home';
-import Blog from './pages/Blog/Blog';
-import Apply from './pages/Apply/Apply';
-import Contact from './pages/Contact/Contact'; 
-import Login from './pages/login&SignIn/LogIn/Login';
-import SignIn from './pages/login&SignIn/SignIn/SignIn';
-import Students from './pages/Students/Students';
-import Teachers from './pages/Teachers/Teachers';
-import Vacancies from './pages/Vacancies/Vacancies';
-import Courses from './pages/Courses/Courses';
-import Event from './pages/Event/Event';
-import Header from './layouts/Header/Header';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import "./index.css";
+import About from "./pages/About/About";
+import Home from "./pages/Home/Home";
+import Blog from "./pages/Blog/Blog";
+import Apply from "./pages/Apply/Apply";
+import Contact from "./pages/Contact/Contact";
+import Login from "./pages/login&SignIn/LogIn/Login";
+import SignIn from "./pages/login&SignIn/SignIn/SignIn";
+import Students from "./pages/Students/Students";
+import Teachers from "./pages/Teachers/Teachers";
+import Vacancies from "./pages/Vacancies/Vacancies";
+import Courses from "./pages/Courses/Courses";
+import Event from "./pages/Event/Event";
+import Header from "./layouts/Header/Header";
 
 function App() {
   return (
-    <Router>
-      {/* Place Header outside Routes to render it on all pages */}
+    <>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/event" element={<Event />} />
-        <Route path="/courses" element={<Courses />} />
-        <Route path="/apply" element={<Apply />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signIn" element={<SignIn />} />
-        <Route path="/student" element={<Students />} />
-        <Route path="/teacher" element={<Teachers />} />
-        <Route path="/vacancies" element={<Vacancies />} />
+        {/* Place Header outside Routes to render it on all pages */}
+        <Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/event" element={<Event />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/apply" element={<Apply />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signIn" element={<SignIn />} />
+          <Route path="/student" element={<Students />} />
+          <Route path="/teacher" element={<Teachers />} />
+          <Route path="/vacancies" element={<Vacancies />} />
+          <Route path="./pages/Contact" element={<Contact/>}/>
+        </Route>
       </Routes>
-    </Router>
+    </>
   );
 }
 
--- src/layouts/Header/Logo.tsx
+++ src/layouts/Header/Logo.tsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import logo from "../../assets/images/logo-black.png"
+import React from "react";
+import logo from "../../assets/images/logo-black.png";
+import { Link } from "react-router-dom";
 
 const Logo = () => {
   return (
-    <div className='logo'>
-      <img src={logo} alt="logo" width={120} className='border-r pr-6 p-1'/>
+    <div className="logo">
+      <Link to="/">
+        <img src={logo} alt="logo" width={120} className="border-r pr-6 p-1" />
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Logo
+export default Logo;
