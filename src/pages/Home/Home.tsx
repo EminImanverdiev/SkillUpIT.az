c060eaab8e5d50ea2058f9a8bdad89cdf727c2cd
Afsana1999 <afsanahasimovaa@gmail.com>
2025-01-04 20:17:37 +0400

    subscribe done

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import CoursesOverview from './CoursesOverview';
-import Funfact from './Funfact';
-import Category from './Category';
+import React from "react";
+import CoursesOverview from "./CoursesOverview";
+import Funfact from "./Funfact";
+import Category from "./Category";
+import Subscribe from "./Subscribe";
+import PopularCourses from "./PopularCourses";
 
 const Home = () => {
   return (
-    <div>   
+    <div>
       <CoursesOverview />
-      <Funfact/>
-      <Category/>
+      <Funfact />
+      <Category />
+      <Subscribe />
+      <PopularCourses />
     </div>
   );
 };
