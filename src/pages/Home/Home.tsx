bc46b78d15fe178e114ef90abb9a115e9b33a709
AlekberliS <117600954+AlekberliS@users.noreply.github.com>
2025-01-04 15:39:24 +0400

    Sssa-3

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,12 +3,8 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <Link
-        to="/contact"
-       
-      >
-       Contact Page
-      </Link>
+      <Link  to="/contact">  Contact Page   </Link>
+      <Link  to="/about">About</Link>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,15 @@ export default {
     extend: {
       colors:{
         'bt-blue':'#5169F1',
-        'grey-cl':'#9FA0A3'
+        'grey-cl':'#9FA0A3',
+        'eightbg':'#f7f3f2',
+        'heading-primary': '#1a73e8',
+        'theme-primary': '#0056b3',
+        'common-white': '#ffffff',
+      },
+      width:{
+        'w-24f':'24%',
+        'w-58f':'58%'
       }
     },
   },
